216bbc84bc4d5a71f98b3ea946b6ed2396887900
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-26 11:20:31 +0530

    [Raj] Added:- The code to add employee data into database

---
+++ ADO.NetEmployeePayRollProgram/AddEmployeeData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetEmployeePayRollProgram
+{
+    /// <summary>
+    /// Created The Class For Adding The Employee Data(UC3)
+    /// </summary>
+    public class AddEmployeeData
+    {
+        //Method for adding data
+        public static EmployeeModel AddData(EmployeeModel employee)
+        {
+            Console.Write("Enter The Name Of The Employee : ");
+            employee.EmployeeName = Console.ReadLine();
+            Console.Write("Enter The Salary Of The Employee : ");
+            employee.BasicPay = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter The Starting Date Of Joining For Employee In yyyy/mm/dd Format: ");
+            employee.StartDate = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Enter The Gender Of The Employee : ");
+            employee.Gender = Convert.ToChar(Console.ReadLine());
+            Console.Write("Enter The Department Of The Employee : ");
+            employee.Department = Console.ReadLine();
+            Console.Write("Enter The Phone Number Of The Employee : ");
+            employee.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter The Address Of The Employee : ");
+            employee.Address = Console.ReadLine();
+            Console.Write("Enter The Deduction Amount Of The Employee : ");
+            employee.Deductions = Convert.ToDouble(Console.ReadLine());           
+            employee.TaxablePay = employee.BasicPay-employee.Deductions;
+            Console.Write("Enter The IncomeTax Amount Of The Employee : ");
+            employee.IncomeTax = Convert.ToDouble(Console.ReadLine());
+            employee.NetPay = employee.TaxablePay - employee.IncomeTax;
+            return employee;
+        }
+    }
+}
--- ADO.NetEmployeePayRollProgram/Program.cs
+++ ADO.NetEmployeePayRollProgram/Program.cs
@@ -13,10 +13,47 @@ namespace ADO.NetEmployeePayRollProgram
         {
             //Displaying the welcome message
             Console.WriteLine("Welcome TO The ADO.Net Employee Payroll Sercive Program");
-            Console.ReadLine();
-
-            //Calling the emplaoyee method to print all the record from the database(UC1&UC2)
-            EmployeeRepository.GetAllEmployees();
+            try 
+            {
+                while (true)
+                {
+                    Console.WriteLine("1: Get All Employee \n2: Get All Employee Using Sql Adapter \n3: Add Employee Details Into DB\n4: Exit");
+                    Console.Write("Enter a choice from above : ");
+                    bool flag = int.TryParse(Console.ReadLine(), out int choice);
+                    if (flag)
+                    {
+                        switch (choice)
+                        {
+                            case 1:
+                                //Calling the employee method to print all the record from the database(UC1&UC2)
+                                EmployeeRepository.GetAllEmployees();
+                                break;
+                            case 2:
+                                //Calling the employee using adapter method to print all the record from the database(UC1&UC2)
+                                EmployeeRepository.GetAllEmployeesUsingDataAdapter();
+                                break;
+                            case 3:
+                                //Creating the object for employee model to add value into db(UC3)
+                                EmployeeModel model = new EmployeeModel();
+                                var empModel = AddEmployeeData.AddData(model);
+                                EmployeeRepository.AddEmployeeIntoDB(empModel);
+                                break;
+                            case 4:
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                Console.WriteLine("Enter right choice");
+                                continue;
+                        }
+                    }
+                    else
+                        Console.WriteLine("Enter some choice");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
